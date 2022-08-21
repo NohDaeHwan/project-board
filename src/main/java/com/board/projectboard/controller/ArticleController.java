b4d0147306d1e190580623f2b15baa05b616564a
NohDaeHwan <ndh5953@naver.com>
2022-08-21 22:35:09 +0900

    #38 - Refactoring: Add of() factory method without meta information to UserAccountDto
    테스트를 용이하게 하기 위해 추가함
    
    테스트 목적 외에도,
    repository 레이어에 영속되지 않은 객체는
    메타 정보가 없는 것이 당연하므로
    새로운 데이터를 만들 때 이 부분을 입력하지 않아도 되는 팩토리 메소드를 만들어주는 것이 의미가 있음

--- src/main/java/com/board/projectboard/controller/ArticleController.java
+++ src/main/java/com/board/projectboard/controller/ArticleController.java
@@ -85,7 +85,7 @@ public class ArticleController {
     public String postNewArticle(ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
-                "eoghks", "eoghks1234", "eoghks@mail.com", "Eoghks", "memo", null, null, null, null
+                "eoghks", "eoghks1234", "eoghks@mail.com", "Eoghks", "memo"
         )));
 
         return "redirect:/articles";
@@ -105,7 +105,7 @@ public class ArticleController {
     public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
-                "uno", "asdf1234", "uno@mail.com", "Uno", "memo", null, null, null, null
+                "eoghks", "eoghks1234", "eoghks@mail.com", "Eoghks", "memo"
         )));
 
         return "redirect:/articles/" + articleId;
